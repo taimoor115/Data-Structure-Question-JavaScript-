53ee5fa7819bbc18132d1935f587bcbb22269369
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-08-06 17:47:54 +0500

    LeetCode-2011

---
+++ Array/FinalValueAfterOperations.js
@@ -0,0 +1,13 @@
+var finalValueAfterOperations = function(operations) {
+  let counter = 0;
+  for(let i = 0; i < operations.length; i++) {
+      if(operations[i] == "X++" || operations[i] == "++X") {
+          counter++;
+      } else {
+          counter--;
+      }
+  }
+  return counter;
+};
+operations = ["--X","X++","X++"]
+console.log(finalValueAfterOperations(operations));
