2bdb8bdb92111d48f76229c927604ac1556a822e
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-11-21 12:51:08 +0500

    2704

---
+++ JavaScript50/ToBeOrNotToBe.js
@@ -0,0 +1,12 @@
+const expect = function (val) {
+  return {
+    toBe: (val2) => {
+      if (val !== val2) throw new Error("Not Equal");
+      else return true;
+    },
+    notToBe: (val2) => {
+      if (val === val2) throw new Error("Equal");
+      else return true;
+    },
+  };
+};
