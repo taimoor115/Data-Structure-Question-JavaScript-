23bfd2e3239c7837312515e3d333ed16b497a3bf
Taimoor Hussain <taimoorhussain985@gmail.com>
2024-04-24 00:36:30 +0500

    LeetCode 28

---
+++ String/Questions/FindtheIndexoftheFirstOccurrenceinaString.js
@@ -0,0 +1,8 @@
+https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+
+var strStr = function(haystack, needle) {
+  return haystack.indexOf(needle);_
+};
+
+
+console.log(strStr("Taimoor", "oor"));
